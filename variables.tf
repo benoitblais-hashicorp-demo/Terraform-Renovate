b1ba51ccb7730868224fea0924a9418ccc1bc199
Benoit Blais <benoit.blais@hashicorp.com>
2026-05-15 14:50:08 -0400

    variable

--- variables.tf
+++ variables.tf
@@ -8,6 +8,6 @@ variable "uuid_source" {
 variable "uuid_version" {
   description = "Version for the uuid module"
   type        = string
-  default     = "~> 1.0"
+  default     = "> 1.0"
   const       = true
 }
