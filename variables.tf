e721a9c9b46b37c5615a960fc62e6397ebdd1871
GitHub Action <notifications@github.com>
2026-05-15 18:47:40 +0000

    Automated Terraform Formatting

--- variables.tf
+++ variables.tf
@@ -2,12 +2,12 @@ variable "uuid_source" {
   description = "Source URL for the uuid module"
   type        = string
   default     = "app.terraform.io/benoitblais-hashicorp/uuid/random"
-  const = true
+  const       = true
 }
 
 variable "uuid_version" {
   description = "Version for the uuid module"
   type        = string
   default     = "1.0"
-  const = true
+  const       = true
 }
